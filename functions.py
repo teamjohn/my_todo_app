ea953fa030817e7e11988c574966773915444a30
Josh N <teamjohn873@gmail.com>
2023-09-14 20:58:29 +0300

    initial commit

---
+++ functions.py
@@ -0,0 +1,15 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return a list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+        return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
